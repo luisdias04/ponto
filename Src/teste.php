7e85f92338690ea8cdc1c9ce8bde91957989efa4
LUIS CARLOS DA SILVA DIAS <luisdias04@users.noreply.github.com>
2021-03-28 11:16:39 -0300

    basicFoderStructure

---
+++ Src/teste.php
@@ -0,0 +1,13 @@
+<?php
+namespace Src;
+
+class testandoNamespace
+{
+private $imprime;
+
+public function rasga(){
+$this->imprime="justo";
+return $this->imprime;
+}
+
+} 
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//namespace Src;
+
+//use testandoNamespace;
+
+//$a=new testandoNamespace;
+//$a->rasga();
+
+echo "ligado";
